4b1cfc6120d6991b4263851d9f9d830c8087df8a
buerotuerk <buerotuerk3203@gmail.com>
2026-04-29 21:19:48 +0200

    Update data-viessmann-gas.js

--- data-viessmann-gas.js
+++ data-viessmann-gas.js
@@ -1,1 +1,14 @@
 
+const dataViessmannGas = [
+  {
+    code: "A01",
+    hersteller: "Viessmann",
+    gruppe: "Gasgerät",
+    geraet: "Vitodens / Gasgerät",
+    baujahr: "-",
+    bedeutung: "Platzhalter Fehlercode",
+    hinweis: "Bitte später mit Viessmann-Unterlagen prüfen."
+  }
+];
+
+alleCodes.push(...dataViessmannGas);
