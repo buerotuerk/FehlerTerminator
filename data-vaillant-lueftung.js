4ad244bf995e7efad603f1c4f82a2b34ff4f0fd3
buerotuerk <buerotuerk3203@gmail.com>
2026-04-29 21:11:36 +0200

    Create data-vaillant-lueftung.js

---
+++ data-vaillant-lueftung.js
@@ -0,0 +1,40 @@
+const dataVaillantLueftung = [
+  {
+    code: "F100",
+    hersteller: "Vaillant",
+    gruppe: "Lüftungsgerät",
+    geraet: "Zentrale Lüftung",
+    baujahr: "2010-2025",
+    bedeutung: "Allgemeiner Gerätefehler",
+    hinweis: "Gerät spannungsfrei schalten und neu starten."
+  },
+  {
+    code: "F101",
+    hersteller: "Vaillant",
+    gruppe: "Lüftungsgerät",
+    geraet: "Zentrale Lüftung",
+    baujahr: "2010-2025",
+    bedeutung: "Lüfterfehler",
+    hinweis: "Ventilator und Verkabelung prüfen."
+  },
+  {
+    code: "F102",
+    hersteller: "Vaillant",
+    gruppe: "Lüftungsgerät",
+    geraet: "Zentrale Lüftung",
+    baujahr: "2010-2025",
+    bedeutung: "Filter verschmutzt",
+    hinweis: "Filter reinigen oder ersetzen."
+  },
+  {
+    code: "F103",
+    hersteller: "Vaillant",
+    gruppe: "Lüftungsgerät",
+    geraet: "Zentrale Lüftung",
+    baujahr: "2015-2025",
+    bedeutung: "Temperatursensor Fehler",
+    hinweis: "Sensor prüfen oder tauschen."
+  }
+];
+
+alleCodes.push(...dataVaillantLueftung);
