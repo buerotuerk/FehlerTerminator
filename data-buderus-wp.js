76fe0479ac7fce4e63c844b2f292c32369c4a920
buerotuerk <buerotuerk3203@gmail.com>
2026-04-29 21:20:09 +0200

    Update data-buderus-wp.js

--- data-buderus-wp.js
+++ data-buderus-wp.js
@@ -1,1 +1,14 @@
 
+const dataBuderusWP = [
+  {
+    code: "A01",
+    hersteller: "Buderus",
+    gruppe: "Wärmepumpe",
+    geraet: "Wärmepumpe",
+    baujahr: "-",
+    bedeutung: "Platzhalter Fehlercode",
+    hinweis: "Bitte später mit Buderus-Unterlagen prüfen."
+  }
+];
+
+alleCodes.push(...dataBuderusWP);
