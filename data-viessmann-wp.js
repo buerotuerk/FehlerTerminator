b6bb76a2c7671a852639683f01a183400c0f6628
buerotuerk <buerotuerk3203@gmail.com>
2026-04-29 21:19:26 +0200

    Update data-viessmann-wp.js

--- data-viessmann-wp.js
+++ data-viessmann-wp.js
@@ -1,1 +1,14 @@
 
+const dataViessmannWP = [
+  {
+    code: "A01",
+    hersteller: "Viessmann",
+    gruppe: "Wärmepumpe",
+    geraet: "Vitocal / Wärmepumpe",
+    baujahr: "-",
+    bedeutung: "Platzhalter Fehlercode",
+    hinweis: "Bitte später mit Viessmann-Unterlagen prüfen."
+  }
+];
+
+alleCodes.push(...dataViessmannWP);
