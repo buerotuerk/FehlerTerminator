3e5d193962514b0fb3d22ca301a98f4a6b834da7
buerotuerk <buerotuerk3203@gmail.com>
2026-04-29 21:18:55 +0200

    Update data-stiebel-wp.js

--- data-stiebel-wp.js
+++ data-stiebel-wp.js
@@ -1,1 +1,32 @@
 
+const dataStiebelWP = [
+  {
+    code: "E10",
+    hersteller: "Stiebel Eltron",
+    gruppe: "Wärmepumpe",
+    geraet: "Luft/Wasser Wärmepumpe",
+    baujahr: "-",
+    bedeutung: "Hochdruckstörung",
+    hinweis: "Kältekreis und Wärmetauscher prüfen."
+  },
+  {
+    code: "E20",
+    hersteller: "Stiebel Eltron",
+    gruppe: "Wärmepumpe",
+    geraet: "Luft/Wasser Wärmepumpe",
+    baujahr: "-",
+    bedeutung: "Niederdruckstörung",
+    hinweis: "Kältemittelmenge und Sensorik prüfen."
+  },
+  {
+    code: "E30",
+    hersteller: "Stiebel Eltron",
+    gruppe: "Wärmepumpe",
+    geraet: "Luft/Wasser Wärmepumpe",
+    baujahr: "-",
+    bedeutung: "Durchfluss zu gering",
+    hinweis: "Pumpe, Filter und Ventile prüfen."
+  }
+];
+
+alleCodes.push(...dataStiebelWP);
