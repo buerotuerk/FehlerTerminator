0964348ce4231c815724f1ca9d579a7e37735b4c
buerotuerk <buerotuerk3203@gmail.com>
2026-04-29 19:14:05 +0200

    Create data-vaillant-wp-luft.js

---
+++ data-vaillant-wp-luft.js
@@ -0,0 +1,32 @@
+const dataVaillantWPLuft = [
+  {
+    code: "F732",
+    bedeutung: "Temperatur am Kompressor-Auslass zu hoch",
+    hinweis: "Kältekreis, Sensorik und Betriebsbedingungen prüfen.",
+    geraet: "Wärmepumpe Luft"
+  },
+  {
+    code: "F733",
+    bedeutung: "Verdampfungstemperatur zu niedrig",
+    hinweis: "Außeneinheit, Luftstrom, Vereisung oder Kältekreis prüfen.",
+    geraet: "Wärmepumpe Luft"
+  },
+  {
+    code: "F731",
+    bedeutung: "Kältemitteldruck zu hoch",
+    hinweis: "Hochdruckstörung. Wärmetauscher / Lüfter prüfen.",
+    geraet: "Wärmepumpe Luft"
+  },
+  {
+    code: "F752",
+    bedeutung: "Frequenzumrichter Fehler",
+    hinweis: "Elektronik / Verdichtersteuerung prüfen.",
+    geraet: "Wärmepumpe Luft"
+  },
+  {
+    code: "F788",
+    bedeutung: "Gebäudekreispumpe Fehler",
+    hinweis: "Pumpe prüfen oder tauschen.",
+    geraet: "Wärmepumpe Luft"
+  }
+];
