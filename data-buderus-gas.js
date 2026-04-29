253bc36a5810866c8ef54a1f2f6a869dce66740c
buerotuerk <buerotuerk3203@gmail.com>
2026-04-29 21:20:27 +0200

    Update data-buderus-gas.js

--- data-buderus-gas.js
+++ data-buderus-gas.js
@@ -1,1 +1,14 @@
 
+const dataBuderusGas = [
+  {
+    code: "A01",
+    hersteller: "Buderus",
+    gruppe: "Gasgerät",
+    geraet: "Gas-Brennwertgerät",
+    baujahr: "-",
+    bedeutung: "Platzhalter Fehlercode",
+    hinweis: "Bitte später mit Buderus-Unterlagen prüfen."
+  }
+];
+
+alleCodes.push(...dataBuderusGas);
